0c4da829b7f137c5c9779ccb66660f3968c6cb9d
kasiauwu558 <kasiauwu558@gmail.com>
2025-09-05 13:05:39 +0200

    Add files via upload

---
+++ d1/z2/kowalewski_zewnetrzny.js
@@ -0,0 +1,5 @@
+document.write("Mój numer w dzienniku to 12 <br>");
+document.write("Pierwszy wiersz to:&nbsp;&nbsp; 5 &nbsp;&nbsp; Tak &nbsp;&nbsp; Tak &nbsp;&nbsp; 7<br> ");
+document.write("Drugi wiersz to:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 6 &nbsp;&nbsp; Nie &nbsp;&nbsp; Tak &nbsp;&nbsp; 6<br> ");;
+document.write("ostatnia modyfikacja strony".fontcolor("maroon").bold().strike().fontsize(7)+"<br>");
+document.write(document.lastModified.fontcolor("red").strike().fontsize(6));
